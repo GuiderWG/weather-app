88ce22bb12c2fc28c389c071eeab617ab82d35e8
Vladimir <guiderwg@gmail.com>
2020-10-05 02:04:18 +0400

    added CSS

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -4,14 +4,36 @@ import style from './Weather.module.css'
 const Weather = (props) => {
   return (
       <div className={style.weather}>
-        {props.places.map((item, index) => {
-          return (
-              <button className={style.city} key={index}>{item.name}</button>
-          )
-        })}
+        <div className={style.toggles}>
+          {props.places.map((item, index) => <button className={style.city} key={index}>{item.name}</button>)}
+        </div>
 
         <div className={style.panel}>
-          123
+          <div className={style.title}><span>Moscow</span>, Russia</div>
+          <div className={style.subtitle}>Time 12:13 a.m.</div>
+          <div className={style.tempInfo}>
+            <div className={style.theTemp}>+18°</div>
+            <img src="https://www.metaweather.com/static/img/weather/hc.svg" alt=""/>
+            <div className={style.condition}>Heavy Cloud</div>
+          </div>
+          <div className={style.additionalInformation}>
+            <div className={style.aiItem}>
+              <div className={style.aiItem__title}>Humidity</div>
+              <div className={style.aiItem__value}>53%</div>
+            </div>
+            <div className={style.aiItem}>
+              <div className={style.aiItem__title}>Visibility</div>
+              <div className={style.aiItem__value}>16.6 miles</div>
+            </div>
+            <div className={style.aiItem}>
+              <div className={style.aiItem__title}>Pressure</div>
+              <div className={style.aiItem__value}>1030mb</div>
+            </div>
+            <div className={style.aiItem}>
+              <div className={style.aiItem__title}>Confidence</div>
+              <div className={style.aiItem__value}>61%</div>
+            </div>
+          </div>
         </div>
       </div>
   )
